5beb073b948a34adb2af76fd8057be47ba632af3
tsukker <tsukker@users.noreply.github.com>
2020-06-07 18:53:09 +0900

    Improve

--- ai.py
+++ ai.py
@@ -1,8 +1,12 @@
 import itertools
+import os
+import pickle
 import random
 import string
 import sys
 import time
+
+cache = dict()
 
 
 def call(s):
@@ -13,17 +17,22 @@ def call(s):
     return ans_tup
 
 
-def groups(mode, candidates, word):
+def calc_match(called, ans):
+    assert len(called) == len(ans)
+    get_cnt = 0
+    touch_cnt = 0
+    for c1, c2 in zip(called, ans):
+        if c1 == c2:
+            get_cnt += 1
+        elif c1 in ans:
+            touch_cnt += 1
+    return (get_cnt, touch_cnt)
+
+
+def groups_of_word(mode, candidates, word):
     ret = dict()
     for cand in candidates:
-        get_cnt = 0
-        touch_cnt = 0
-        for i in range(mode):
-            if word[i] == cand[i]:
-                get_cnt += 1
-            elif word[i] in cand:
-                touch_cnt += 1
-        key = (get_cnt, touch_cnt)
+        key = calc_match(word, cand)
         if key not in ret:
             ret[key] = []
         ret[key].append(cand)
@@ -33,9 +42,41 @@ def groups(mode, candidates, word):
 def calc_score(mode, candidates, word):
     if word[1:] == 'ABC':
         print(f'calc_score {word}', file=sys.stderr)
-    gs = groups(mode, candidates, word)
+    gs = groups_of_word(mode, candidates, word)
     sizes = [len(val) for val in gs.values()]
-    return (max(sizes), word)
+    sizes = sorted(sizes, reverse=True)
+    return (sizes, word)
+
+
+def select_for_best(results):
+    best = min(results)
+    sizes, word = best
+    if word[0] == '!':
+        word = word[1:]
+    return (sizes, word)
+
+
+def search_all_words(mode, candidates):
+    global cache
+    fs_cand = frozenset(candidates)
+    if fs_cand in cache:
+        print('cache hit !!')
+        return cache[fs_cand]
+    l = 1
+    for i in range(mode):
+        l *= (26 - i)
+    results = [None] * l
+    for i, word in enumerate(
+            map(lambda tup: ''.join(tup),
+                itertools.permutations(string.ascii_uppercase, mode))):
+        results[i] = calc_score(mode, candidates, word)
+        if word in fs_cand:
+            tmp, _ = results[i]
+            results[i] = (tmp, f'!{word}')
+    ret = sorted(results)[:100]
+    if 40 < len(candidates):
+        cache[fs_cand] = ret
+    return ret
 
 
 def turn(mode, candidates, word=None):
@@ -52,19 +93,24 @@ def turn(mode, candidates, word=None):
         best_word = word
         best_score = calc_score(mode, candidates, best_word)
     else:
-        l = 1
-        for i in range(mode):
-            l *= (26 - i)
-        results = [None] * l
-        for i, word in enumerate(
-                map(lambda tup: ''.join(tup),
-                    itertools.permutations(string.ascii_uppercase, mode))):
-            results[i] = calc_score(mode, candidates, word)
-        best_score, best_word = min(results)
+        results = search_all_words(mode, candidates)
+        best_score, best_word = select_for_best(results)
     print(f'word: {best_word}, score: {best_score}', file=sys.stderr)
-    best_groups = groups(mode, candidates, best_word)
+    best_groups = groups_of_word(mode, candidates, best_word)
     key = call(best_word)
-    next_candidates = best_groups[key]
+    next_candidates = None
+    while True:
+        try:
+            next_candidates = best_groups[key]
+            break
+        except:
+            print('Got unexpected answer. Finish? y/n: ', end='', flush=True)
+            line = input()
+            if line == 'y':
+                dump_cache(mode)
+                sys.exit()
+            else:
+                key = call(best_word)
     if key == (4, 0):
         next_candidates = []
     return next_candidates
@@ -78,30 +124,48 @@ def read_words(filepath):
 
 
 def generate_answer_word(mode, all_words):
-    if mode == 4:
-        ng_words = read_words('ng_word_4.txt')
-        candidates = list(set(all_words) - set(ng_words))
-        answer_word = random.choice(candidates)
-        return answer_word
-    assert False
+    ng_words = read_words(f'ng_word_{mode}.txt')
+    candidates = list(set(all_words) - set(ng_words))
+    answer_word = random.choice(candidates)
+    return answer_word
+
+
+def load_cache(mode):
+    global cache
+    filepath = f'cache_{mode}.pickle'
+    if not os.path.isfile(filepath):
+        cache = dict()
+        return
+    with open(filepath, 'rb') as f:
+        cache = pickle.load(f)
+    return
+
+
+def dump_cache(mode):
+    global cache
+    filepath = f'cache_{mode}.pickle'
+    with open(filepath, 'wb') as f:
+        pickle.dump(cache, f)
 
 
 if __name__ == '__main__':
-    words = read_words('dictionary.txt')
     argc = len(sys.argv)
     mode = 4
     if argc >= 2:
         if sys.argv[1] == '3':
             mode = 3
-    all_words = [word for word in words if len(word) == mode]
+    load_cache(mode)
+    all_words_34 = read_words('dictionary.txt')
+    all_words = [word for word in all_words_34 if len(word) == mode]
     print(generate_answer_word(mode, all_words), flush=True)
     candidates = all_words
     first_word = None
     try:
-        first_word = {4: 'DARE'}[mode]
+        first_word = random.choice(read_words(f'first_word_{mode}.txt'))
     except:
         pass
     candidates = turn(mode, candidates, first_word)
     while (len(candidates) > 0):
         candidates = turn(mode, candidates)
+    dump_cache(mode)
     print('finish')
